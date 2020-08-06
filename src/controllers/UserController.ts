02c745003e25a10ac7d1dd7c62c5fe67fec49350
Lusca Lima <lucas@siasp.com.br>
2020-08-05 21:40:47 -0300

    Creared the route to fetch a sigle user by id

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -16,6 +16,32 @@ class UserController {
         res.status(400).send(err)
       })
   }
+
+  public all(_: Request, res: Response) {
+    User.find({})
+      .then(users => {
+        res.json(users)
+      })
+      .catch(() => {
+        res.status(500).send()
+      })
+  }
+
+  public oneById(req: Request, res: Response) {
+    const { id } = req.params
+
+    User.findById(id)
+      .then(user => {
+        if (!user) {
+          return res.status(404).send()
+        }
+
+        return res.json(user)
+      })
+      .catch(err => {
+        res.status(404).send(err)
+      })
+  }
 }
 
 export default new UserController()
--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -5,4 +5,10 @@ import User from '../controllers/UserController'
 /** Criation route */
 router.post('/', User.create)
 
+/** Get all users */
+router.get('/', User.all)
+
+/** Get one user by ID */
+router.get('/:id', User.oneById)
+
 export default router
