be4338352e2834b196a8a4692bcba60d0eae7843
Lusca Lima <lucas@siasp.com.br>
2020-08-15 22:42:20 -0300

    feat(middlewares): creating the authetication middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,55 @@
+import { Request, Response, NextFunction } from 'express'
+import { IUser } from '../interfaces/User'
+import jwt from 'jsonwebtoken'
+import User from '../models/User'
+
+/** Specific local interface for Request object */
+interface IRequest extends Request {
+  user?: IUser
+}
+
+/** Make the HASH env variable visible as a string */
+declare const process: {
+  env: {
+    HASH: string
+  }
+}
+
+/** For casting the data to get the '_id' attr */
+type decoded = {
+  _id: string
+}
+
+/** This function is executed in every route that takes authtentication */
+export async function auth(
+  req: IRequest,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  try {
+    const token = req.headers.authorization?.replace('Bearer ', '')
+
+    // This validation is a simple mode to avoid the TS typecheking
+    if (!token) {
+      throw new Error()
+    }
+
+    const decoded = <decoded>jwt.verify(token, process.env.HASH)
+
+    const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+
+    if (!user) {
+      throw new Error()
+    }
+
+    // Prevents this same user from being brought back from
+    // the database again in the handler of the route
+    req.user = user
+
+    next()
+  } catch (e) {
+    res.status(401).json({
+      error: 'Please, authenticate first'
+    })
+  }
+}
