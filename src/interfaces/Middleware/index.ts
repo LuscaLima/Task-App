dcede153044af468b33e996218a55c08cb5f3a05
Lusca Lima <lucas@siasp.com.br>
2020-08-15 23:01:57 -0300

    feat(interface): adding the custom interfaces for the middlewares

---
+++ src/interfaces/Middleware/index.ts
@@ -0,0 +1,7 @@
+import { Request } from 'express'
+import { IUser } from '../User'
+
+/** Specific local interface for Request object */
+export interface IRequest extends Request {
+  user?: IUser
+}
