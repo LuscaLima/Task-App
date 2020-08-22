13ae856e6cf94a27a434a92aa9ba4405091b60f4
Lusca Lima <lusca.lima@outlook.com>
2020-08-22 18:38:49 -0300

    feat: add multer

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import './database'
-import UserRoutes from './routes/UserRoutes'
+import { errorHandler } from './middlewares/general'
+import UserRoutes, from './routes/UserRoutes'
 import TaskRoutes from './routes/TaskRoutes'
 
 /** Instantiate the express and define the port to listen */
@@ -10,6 +11,7 @@ const port = process.env.PORT || 3000
 /** All basic settings for express */
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(errorHandler)
 
 /** Setup all routes associate with the task route */
 app.use('/task', TaskRoutes)
